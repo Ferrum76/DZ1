51e3c9fc306be0493430f54f8d806f19d95bf942
ferrum76 <daniil.zheleznyakov.2000@gmail.com>
2024-04-01 20:35:26 +0300

    add setter getter and property methods

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ class Category:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.products = []
+        self.__products = []
         Category.total_categories += 1
 
     def add_product(self, product):
-        self.products.append(product)
+        self.__products.append(product)
         Category.total_unique_products.add(product.name)
 
     def remove_product(self, product):
-        self.products.remove(product)
+        self.__products.remove(product)
         Category.total_unique_products.remove(product.name)
+
+    def get_products(self):
+        return self.__products
+
+    def get_products_info(self):
+        products_info = []
+        for product in self.__products:
+            info = f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+            products_info.append(info)
+        return products_info
 
 
 class Product:
@@ -23,3 +33,19 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def create_product(cls, name, description, price, quantity):
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена введена некорректная")
+        else:
+            self.__price = new_price
+
