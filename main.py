cf737d989e94456c51a855101ad2ca6734268b98
ferrum76 <daniil.zheleznyakov.2000@gmail.com>
2024-04-15 21:55:42 +0300

    fixed to comments

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 class Category:
     total_categories = 0
-    total_unique_products = set()
+    all_unique_products = set()  # Global tracking of unique products
 
-    def __init__(self, name, description):
+    def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = []
+        self.products = list(set(products))  # Ensures unique products at initialization
         Category.total_categories += 1
+        Category.all_unique_products.update(self.products)
+
 
     def add_product(self, product):
-        self.products.append(product)
-        Category.total_unique_products.add(product.name)
+        if product not in self.products:
+            self.products.append(product)
+            Category.all_unique_products.add(product)
 
     def remove_product(self, product):
-        self.products.remove(product)
-        Category.total_unique_products.remove(product.name)
+        if product in self.products:
+            self.products.remove(product)
 
+    def get_total_unique_products():
+        return len(Category.all_unique_products)
 
 class Product:
     def __init__(self, name, description, price, quantity):
