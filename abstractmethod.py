eaa1d5854b7aeea5d9c1e02f2d3a3598b305f060
ferrum76 <daniil.zheleznyakov.2000@gmail.com>
2024-04-16 12:14:02 +0300

    fixed abstract class

--- abstractmethod.py
+++ abstractmethod.py
@@ -2,36 +2,22 @@ from abc import ABC, abstractmethod
 
 class ObjectCreationMixin:
     def __repr__(self):
-        class_name = self.__class__.__name__
         attributes = ', '.join(f"{attr}={getattr(self, attr)}" for attr in self.__dict__)
-        return f"{class_name}({attributes})"
+        return f"{self.__class__.__name__}({attributes})"
 
 class AbstractProduct(ABC):
+
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.__price = price
+        self.price = price
         self.quantity = quantity
 
-    @property
-    def price(self):
-        return self.__price
-
-    @price.setter
-    def price(self, new_price):
-        if new_price <= 0:
-            print("Цена введена некорректная")
-        else:
-            self.__price = new_price
+    @abstractmethod
+    def __str__(self):
+        pass
 
     @abstractmethod
     def get_additional_info(self):
         pass
-
-    @abstractmethod
-    def new_product(self, *args):
-        pass
-
-    def __str__(self):
-        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
